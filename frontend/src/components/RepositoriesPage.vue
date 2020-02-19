95f86f0e569a1de6644dd19e63c78c6e4ee65995
Vit Stanislav <slaweet@gmail.com>
2020-02-19 21:02:00 +0100

    Implement more styling of the RepositoriesPage

--- frontend/src/components/RepositoriesPage.vue
+++ frontend/src/components/RepositoriesPage.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
+    <!-- TODO move menu to its own component -->
     <h2 class="menu-item">
       <Octicon :icon="repo" />
       Repositories
     </h2>
-    <div>
+    <div class="box">
       <Octicon :icon="search" />
-     <input type="text" v-model="filter" placeholdeer="Search files">
+     <input class="filter" type="text" v-model="filter" placeholder="Search files">
     </div>
 
-    <div v-for="repository in filteredRepos" v-bind:key="repository.name" >
-      <RepositoryRow v-bind:repository="repository" />
+    <div class="box"> <!-- TODO make box its own component -->
+      <div v-for="repository in filteredRepos" v-bind:key="repository.name" >
+        <RepositoryRow v-bind:repository="repository" />
+      </div>
     </div>
   </div>
 </template>
@@ -47,6 +50,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  input.filter {
+    font-family: Lato;
+    font-size: 16px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: italic;
+    line-height: 1.19;
+    letter-spacing: normal;
+    width: 90%;
+    padding: 10px;
+    border: 0;
+    color: #0e1b29; /* TODO use variables for colors */
+  }
+
   .menu-item {
     font-family: Lato;
     font-size: 14px;
@@ -60,5 +77,13 @@ export default {
     padding: 12px;
     border-bottom: solid 2px #394754;
     display: inline-block;
+  }
+
+  .box {
+    border-radius: 5px;
+    box-shadow: 0 2px 15px 0 rgba(14, 27, 41, 0.05);
+    background-color: #ffffff;
+    margin-top: 10px;
+    padding: 20px 30px;
   }
 </style>
